4c3f9e7c0409ca3ef671fc4be8555aaa83b88ad6
nuria10080 <nuria10080@gmail.com>
2026-05-03 20:21:10 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker for offline support
 
-const CACHE_NAME = 'Iniciativa de Voluntariado-Informe-Tecnico-v1.1.1'; // 🔥 CHANGE THIS on every deploy
+const CACHE_NAME = 'Iniciativa de Voluntariado-Informe-Tecnico-v1.1.2'; // 🔥 CHANGE THIS on every deploy
 
 const urlsToCache = [
     './',
